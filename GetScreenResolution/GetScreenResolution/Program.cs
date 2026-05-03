98cd7f550df0f64fc242184d3d910f2d10ea23d2
moha327 <m7amad9595@outlook.com>
2026-05-03 17:39:19 +0300

    GetScreenResolution

---
+++ GetScreenResolution/GetScreenResolution/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetScreenResolution
+{
+    internal class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(int nIndex);
+        const int SM_CXSCREEN = 0; // screen width
+        const int SM_CYSCREEN = 1; // screen height
+        static void Main(string[] args)
+        {
+            int width = GetSystemMetrics(SM_CXSCREEN);
+            int height = GetSystemMetrics(SM_CYSCREEN);
+            Console.WriteLine($"Resolution: {width} x {height}");
+        }
+    }
+}
