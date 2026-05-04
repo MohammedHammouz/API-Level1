83e92e64cde26fabc1ae511bb48ee1130c79b44b
moha327 <m7amad9595@outlook.com>
2026-05-04 12:40:04 +0300

    MessageBox

---
+++ MessageBox/MessageBox/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBox
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode,SetLastError =true)]
+        public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+        static void Main(string[] args)
+        {
+            MessageBox(IntPtr.Zero, "Mohammed Hammouz", "My name's", 0);
+        }
+    }
+}
